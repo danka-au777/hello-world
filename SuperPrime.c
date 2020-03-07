4c64cc7a54637813b06aa41261998f5066eaf2a0
danka-au777 <keep_au777@outlook.com>
2020-03-07 22:34:15 +0800

    assignment1 SuperPrime commit

--- SuperPrime.c
+++ SuperPrime.c
@@ -1,10 +1,55 @@
-//è¶…çº§ç´ æ•°ï¼šå®ƒæœ¬èº«ï¼Œå„ä½æ•°å­—çš„å’Œï¼Œå„ä½æ•°å­—çš„å¹³æ–¹å’Œï¼Œéƒ½æ˜¯ç´ æ•°ã€‚
-//æ±‚ 100~10000 å†…çš„æ‰€æœ‰è¶…çº§ç´ æ•°ï¼ŒåŠå®ƒä»¬çš„å¹³å‡æ•°ã€‚
-
+//³¬¼¶ËØÊı£ºËü±¾Éí£¬¸÷Î»Êı×ÖµÄºÍ£¬¸÷Î»Êı×ÖµÄÆ½·½ºÍ£¬¶¼ÊÇËØÊı¡£
+//Çó 100~10000 ÄÚµÄËùÓĞ³¬¼¶ËØÊı£¬¼°ËüÃÇµÄÆ½¾ùÊı¡£
+#include<stdio.h>
 int isPrime(int x);
 int splitNum(int x, int num[]);
 
 int main()
 {
+	int i,k=0,size,j,sum,sqrsum;
+	int ans[1000],bit[5];
+	float SUM=0.0,Ave;
+	for(i=100;i<10001;i++){
+		if(isPrime(i)==0){
+		   sum=0;sqrsum=0;
+		   size=splitNum(i,bit);
+		   for(j=0;j<size;j++){
+			   sum=sum+bit[j];
+			   sqrsum=sqrsum+bit[j]*bit[j];
+		    }
+		   if(isPrime(sum)==0&&isPrime(sqrsum)==0){
+			   ans[k]=i; k++;
+		    }
+		}
+	}
+	printf("The superprimes between 100 and 10000 are:");
+	for(i=0;i<k;i++){
+		printf("%d ",ans[i]);
+		SUM+=ans[i];
+	}
+	printf("\n");
+	Ave=SUM/k;
+	printf("The average is %f\n",Ave);
     return 0;
+}
+
+int isPrime(int x){
+	int i,flag=0;
+	for(i=2;i<x;i++){
+		if(x%i==0){
+		  flag=1;break;
+		}	
+	}
+	if(x==2)flag=0;
+	return flag;
+ } 
+ 
+int splitNum(int x,int num[]){
+	int i,j=1;
+	for(i=0;x!=0;i++){
+		j*=10;
+		num[i]=(x%j)/(j/10);
+		x-=num[i]*(j/10);
+	}
+	return i;
 }
